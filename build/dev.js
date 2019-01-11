ac577a7cf5bc6906f337ccc3f93220b895ef8046
zhzxang <xuzhang3371@163.com>
2019-01-11 11:13:34 +0800

    fix: 更新 webpack 配置

--- build/dev.js
+++ build/dev.js
@@ -7,60 +7,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const root = fs.realpathSync(process.cwd())
 const reslove = (dir) => path.resolve(root, dir)
 
-module.exports = {
-  entry: reslove('example/index.js'),
-  output: {
-    path: reslove('docs'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      styles: reslove('example/styles')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|tsx|ts|js)$/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(styl|css)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                postcssPresetEnv({
-                  autoprefixer: { grid: true }
-                })
-              ]
-            }
-          },
-          'stylus-loader'
-        ]
-      },
-      {
-        test: /\.(svg|png|ttf|woff|eot)$/,
-        use: 'url-loader'
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development'
+  return {
+    entry: reslove('example/index.js'),
+    output: {
+      path: reslove('docs'),
+      publicPath: isDev ? '/' : './',
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        styles: reslove('example/styles')
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|tsx|ts|js)$/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(styl|css)$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  postcssPresetEnv({
+                    autoprefixer: { grid: true }
+                  })
+                ]
+              }
+            },
+            'stylus-loader'
+          ]
+        },
+        {
+          test: /\.(svg|png|ttf|woff|eot)$/,
+          use: 'url-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: reslove('public/index.html')
+      }),
+      new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: reslove('public/index.html')
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  }
 }
